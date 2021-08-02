447200e44e1e9065d7de0e9ddf16a2e690e57aa9
ryoma-k63 <ryoma.k.63@gmail.com>
2021-08-02 21:48:23 +0900

    timerとloggerの実装を分離した

---
+++ Src/utility/timer.cpp
@@ -0,0 +1,13 @@
+﻿#include "utility/timer.h"
+
+void lycoris::utility::timer::start()
+{
+	QueryPerformanceCounter(&started_at_);
+}
+
+double lycoris::utility::timer::stop()
+{
+	LARGE_INTEGER stopped_at;
+	QueryPerformanceCounter(&stopped_at);
+	return static_cast<double>(stopped_at.QuadPart - started_at_.QuadPart) / static_cast<double>(frequency.QuadPart);
+}
