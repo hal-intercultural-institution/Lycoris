34006ef110ef7c19f6e1ec720920bdba6c563957
ryoma-k63 <ryoma.k.63@gmail.com>
2021-09-02 14:16:26 +0900

    overlayにカメラ関連を追加

--- Src/profiler/overlay.cpp
+++ Src/profiler/overlay.cpp
@@ -16,13 +16,19 @@ void lycoris::profiler::debug_overlay::on_draw()
 	if (!shown_) return;
 	auto& game = game::get_game();
 	const auto text = std::format(
-		L"Lycoris\nfps: {}\nTick: {:f}\nDraw: {:f}\nMouse x: {}\nMouse y: {}\nGame-pads: {}",
+		L"Lycoris\nfps: {}\nTick: {:f}\nDraw: {:f}\nMouse x: {}\nMouse y: {}\nGame-pads: {}\nCamera pos: {} {} {}\nCamera rot: {} {}\nCamera distance: {}",
 		game.get_fps_last_second(),
 		game.get_last_tick_time(),
 		game.get_last_draw_time(),
 		game.get_input_system().get_mouse().get_screen_x(),
 		game.get_input_system().get_mouse().get_screen_y(),
-		game.get_input_system().get_game_pad_count()
+		game.get_input_system().get_game_pad_count(),
+		game.get_renderer().get_camera().get_position().x,
+		game.get_renderer().get_camera().get_position().y,
+		game.get_renderer().get_camera().get_position().z,
+		game.get_renderer().get_camera().get_rotation().x,
+		game.get_renderer().get_camera().get_rotation().y,
+		game.get_renderer().get_camera().get_camera_distance()
 	);
 	game.get_renderer().draw_text(text);
 }
