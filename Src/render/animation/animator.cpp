9ccf9c57271f1063d84869001b5d2d79f31ed644
ryoma-k63 <ryoma.k.63@gmail.com>
2021-11-18 02:54:45 +0900

    アニメーションのデータ保存部分だけ作った

---
+++ Src/render/animation/animator.cpp
@@ -0,0 +1,22 @@
+﻿#include "render/animation.h"
+
+#include <stdexcept>
+
+lycoris::render::animation::animator::operator bool() const
+{
+	return !animations_.empty();
+}
+
+void lycoris::render::animation::animator::set_frame(const float frame)
+{
+	if (frame >= 0.0f) throw std::invalid_argument("Animator: negative value is not allowed here");
+	if (animations_.empty()) throw std::runtime_error("Animator: animation data are not loaded");
+	frame_ = frame;
+
+	for (auto& animation : animations_)
+	{
+		// キーフレが1つ以下なものはアニメーションではないので何もしない
+		if (animation->size() <= 1) continue;
+		animation.set_frame(frame);
+	}
+}
