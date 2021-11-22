1c00b4b2a1d77cf7616498d05d5944e5241bb613
ryoma-k63 <ryoma.k.63@gmail.com>
2021-11-22 13:17:35 +0900

    constants.hの改行を修正

--- pre-build-task.bat
+++ pre-build-task.bat
@@ -5,7 +5,8 @@ FOR /F "tokens=*" %%b in ('git rev-parse --abbrev-ref HEAD') do SET BRANCH=%%b
 
 echo #pragma once > %FILENAME%
 echo. >> %FILENAME%
-echo namespace lycoris::constants { >> %FILENAME%
+echo namespace lycoris::constants >> %FILENAME%
+echo { >> %FILENAME%
 echo    constexpr wchar_t commit_id[] = L"%COMMIT%"; >> %FILENAME%
 echo    constexpr wchar_t branch_name[] = L"%BRANCH%"; >> %FILENAME%
 echo    constexpr wchar_t configuration_name[] = L"%2"; >> %FILENAME%
