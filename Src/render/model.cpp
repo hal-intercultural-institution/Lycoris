e221f59444e745387500008b7bdae68be20a9e6a
ryoma-k63 <ryoma.k.63@gmail.com>
2021-11-25 00:58:14 +0900

    animated_modelの描画を実装

--- Src/render/model.cpp
+++ Src/render/model.cpp
@@ -46,6 +46,49 @@ void lycoris::render::model3d::model_3d::draw(const DirectX::XMFLOAT3& position,
 
 }
 
+void lycoris::render::model3d::animated_model::draw(const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT3& scale,
+	const DirectX::XMFLOAT3& rotation, const animation::animator& animator)
+{
+	auto& renderer = game::get_game().get_renderer();
+	auto& device_context = renderer.get_device_context();
+
+	auto world_matrix = XMMatrixMultiply(DirectX::XMMatrixIdentity(), DirectX::XMMatrixScalingFromVector(XMLoadFloat3(&scale)));
+	world_matrix = XMMatrixMultiply(world_matrix, DirectX::XMMatrixRotationRollPitchYawFromVector(XMLoadFloat3(&rotation)));
+	world_matrix = XMMatrixMultiply(world_matrix, DirectX::XMMatrixTranslationFromVector(XMLoadFloat3(&position)));
+
+	DirectX::XMFLOAT4X4 world_matrix_float{};
+	XMStoreFloat4x4(&world_matrix_float, world_matrix);
+
+	renderer.set_depth_enabled(true);
+	renderer.set_world_matrix(world_matrix_float);
+	renderer.set_culling_mode(D3D11_CULL_NONE);
+	renderer.set_uv_offset({ 0.0f, 0.0f });
+	renderer.set_animation(animator);
+	renderer.set_vertex_shader(shader::vertex::animated);
+
+	constexpr auto stride = static_cast<uint32_t>(sizeof(animated_vertex));
+	constexpr std::uint32_t offset = 0;
+	const std::array vertex_buffers = {
+		vertex_buffer_.get()
+	};
+
+	device_context.IASetVertexBuffers(0, static_cast<std::uint32_t>(vertex_buffers.size()), vertex_buffers.data(), &stride, &offset);
+
+	device_context.IASetIndexBuffer(index_buffer_.get(), DXGI_FORMAT_R32_UINT, 0);
+
+	device_context.IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	for (auto& [texture, material, start_index, indices] : materials_)
+	{
+		renderer.set_material(material);
+		const std::array srvs = {
+			texture.get_shader_resource_view()
+		};
+		device_context.PSSetShaderResources(0, static_cast<std::uint32_t>(srvs.size()), srvs.data());
+		device_context.DrawIndexed(indices, start_index, 0);
+	}
+}
+
 void lycoris::render::model3d::draw_model(const model_3d& model)
 {
 	auto& renderer = game::get_game().get_renderer();
