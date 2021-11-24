9f84ca883230d15f79f39d4f36f820fa65a3f7cd
ryoma-k63 <ryoma.k.63@gmail.com>
2021-11-24 23:55:32 +0900

    animated_modelを追加

--- Include/render/model3d.h
+++ Include/render/model3d.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 
+#include "animation.h"
 #include "render/renderer.h"
 #include "render/texture.h"
 
@@ -56,6 +57,43 @@ namespace lycoris::render::model3d
 		}
 
 		void draw(const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT3& scale, const DirectX::XMFLOAT3& rotation);
+
+	private:
+		winrt::com_ptr<ID3D11Buffer> vertex_buffer_, index_buffer_;
+		std::vector<model_material> materials_{};
+	};
+
+	class animated_model
+	{
+	public:
+		animated_model() = default;
+		explicit animated_model(winrt::com_ptr<ID3D11Buffer> && vertex_buffer, winrt::com_ptr<ID3D11Buffer> && index_buffer, std::vector<model_material> && materials)
+		{
+			vertex_buffer_ = std::move(vertex_buffer);
+			index_buffer_ = std::move(index_buffer);
+			materials_ = std::move(materials);
+		}
+		animated_model(const animated_model&) = delete;
+		animated_model(animated_model&&) = default;
+		~animated_model() = default;
+
+		animated_model& operator=(const animated_model&) = delete;
+		animated_model& operator=(animated_model&&) = default;
+
+		ID3D11Buffer* get_vertex_buffer() const
+		{
+			return vertex_buffer_.get();
+		}
+		ID3D11Buffer* get_index_buffer() const
+		{
+			return index_buffer_.get();
+		}
+		const std::vector<model_material>& get_materials() const
+		{
+			return materials_;
+		}
+
+		void draw(const DirectX::XMFLOAT3& position, const DirectX::XMFLOAT3& scale, const DirectX::XMFLOAT3& rotation, const animation::animator& animator);
 
 	private:
 		winrt::com_ptr<ID3D11Buffer> vertex_buffer_, index_buffer_;
