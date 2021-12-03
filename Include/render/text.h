02715a9abe8e66138a038437ff149a5d46c4421d
ryoma-k63 <ryoma.k.63@gmail.com>
2021-12-04 01:18:00 +0900

    constを外した

--- Include/render/text.h
+++ Include/render/text.h
@@ -12,7 +12,7 @@ namespace lycoris::render
 		text_format() = default;
 		explicit text_format(winrt::com_ptr<IDWriteTextFormat>&& ptr);
 
-		const IDWriteTextFormat& get() const;
+		IDWriteTextFormat& get() const;
 
 	private:
 		winrt::com_ptr<IDWriteTextFormat> d_write_text_format_;
@@ -24,7 +24,7 @@ namespace lycoris::render
 		text_color() = default;
 		explicit text_color(winrt::com_ptr<ID2D1SolidColorBrush>&& ptr);
 
-		const ID2D1SolidColorBrush& get() const;
+		ID2D1SolidColorBrush& get() const;
 
 	private:
 		winrt::com_ptr<ID2D1SolidColorBrush> brush_;
--- Src/render/text.cpp
+++ Src/render/text.cpp
@@ -5,7 +5,7 @@ lycoris::render::text_format::text_format(winrt::com_ptr<IDWriteTextFormat>&& pt
 	d_write_text_format_ = std::move(ptr);
 }
 
-const IDWriteTextFormat& lycoris::render::text_format::get() const
+IDWriteTextFormat& lycoris::render::text_format::get() const
 {
 	assert(d_write_text_format_);
 	return *d_write_text_format_.get();
@@ -16,7 +16,7 @@ lycoris::render::text_color::text_color(winrt::com_ptr<ID2D1SolidColorBrush>&& p
 	brush_ = std::move(ptr);
 }
 
-const ID2D1SolidColorBrush& lycoris::render::text_color::get() const
+ID2D1SolidColorBrush& lycoris::render::text_color::get() const
 {
 	assert(brush_);
 	return *brush_.get();
