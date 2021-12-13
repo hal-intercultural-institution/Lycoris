6ca10788c3f1568a369730c8e62807509725ed0c
ryoma-k63 <ryoma.k.63@gmail.com>
2021-12-13 17:05:45 +0900

    clipする色を間違えた

--- Shader/ps_alpha.hlsl
+++ Shader/ps_alpha.hlsl
@@ -4,6 +4,6 @@
 float4 ps_alpha(const vs_out input) : SV_TARGET
 {
     float4 output = texture0.Sample(sampler0, input.texel) * input.color;
-    clip(output.z - 0.001f);
+    clip(output.a - 0.001f);
     return output;
 }
